a5012080fe9cf178c2a6c7944f0423a25636888d
nazrinshukurova <nezrinnshukurova23>
2025-02-18 16:39:51 +0400

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Data from "./components/Data/Data";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 import PopUpCard from "./components/PopUpCard/PopUpCard";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Data />
-      <Products/>
-      {/* <PopUpCard/> */}
+      <Products />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="button-div">
+      <div className="showMoreProduct">Show More Product</div>
+    </div>
+  );
+};
+
+export default Footer;
